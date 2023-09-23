83a4e099da10f418980bc2aff1c19450af429e73
Xenon <143745702+XenonOrSomething@users.noreply.github.com>
2023-09-23 17:55:34 +0100

    add some final touches 5 minutes before it starts lol

--- scripts.js
+++ scripts.js
@@ -3,38 +3,28 @@
 
 console.log("WE ARE VR!");
 
-function parse(str) {
-    var args = [].slice.call(arguments, 1),
-        i = 0;
 
-    return str.replace(/%s/g, () => args[i++]);
-}
-
-
-setTimeout(function() {
-
-var response = fetch("https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/?appid=1533390")
-    .then(response => response.json()).then(console.log)
-    .catch(error => {
-        console.error('error fetching json:', error);
-    })
-        ;
-     
-     console.log(response);
-    setTimeout(9999);
-    jsonToObject = JSON.parse(response);
-    var playerCount = response.player_count;
-    var result = response.result;
-    var whythefuckdoesthisnotwork = toString(result)
-
-}, 2000)
+  $(document).ready(function() {
+    var audio = document.getElementById("tick");
+    audio.play();
+    $.ajax({
+      url: "https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v0001/?appid=1533390", 
+      dataType: "json",
+      success: function(data) {
+        
+        var desiredData = data.response.player_count; 
+        
+        
+        console.log(desiredData);
+        $("#playercount").html("<p>" + "There is currently " + desiredData + " players online on Steam alone!" + "</p>");
+      },
+      error: function() {
+        
+        $("#playercount").html("<p>Error loading player count</p>");
+      }
+    });
+  
+  })
 
 
-
-
-var playerText = whythefuckdoesthisnotwork;
-
-document.getElementById("playercount").innerHTML = playerText;
-
-console.log("undefined")
 
